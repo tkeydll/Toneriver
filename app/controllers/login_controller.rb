71bfd2b5275335244a2ebbe8755ce60dbd0b14f8
tkey.dll <tkey.dll@gmail.com>
2014-11-01 22:41:43 +0900

    Add comment.

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -9,14 +9,18 @@
     reset_session
     @user = User.new(params[:user])
 
+    # 認証
+    # なにをやっているのか、さっぱりわからん。
     @auth = User.find_by_login_and_password(params[:user][:login], params[:user][:password])
 
     respond_to do |format|
       unless @auth
         flash[:notice] = "ログイン名またはパスワードが違います。"
+        # index.html を表示
         format.html { render :action => "index" }
       else
         session[:user] = @auth.id
+        # top にリダイレクト
         format.html { redirect_to(top_index_path) }
       end
     end
