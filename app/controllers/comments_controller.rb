0e79f56c1b9994825af576a479c76fe05cae8869
tkey.dll <tkey.dll@gmail.com>
2014-11-03 20:16:02 +0900

    Imprement user comment.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,20 +5,28 @@ class CommentsController < ApplicationController
   # POST /comments
   # POST /comments.json
   def create
-    # ↓ここにテキストの解説(p54～55)に従ってコードを追加します
+    item = Item.find(params[:id])
+    comment = Comment.new(params[:comment])
 
+    comment.user = @me
+    item.comments << comment
 
-
-
-
-
+    respond_to do |format|
+      if item.save
+        format.html { redirect_to top_path(item), notice: "#{@me.name}さんのレビューコメントが投稿されました" }
+        format.json { render json: comment, staus: created, location: comment }
+      else
+        format.html { redirect_to top_path(item) }
+        format.json { render json: comment.errors, status: unprocessable_entity }
+      end
+    end
 
   end
 
   # DELETE /comments/1
   # DELETE /comments/1.json
   def destroy
-    # ↓ここにテキストの解説(p58)に従ってコードを追加します
+    # ↓item),ここにテキストの解説(p58)に従ってコードを追加します
 
 
 
